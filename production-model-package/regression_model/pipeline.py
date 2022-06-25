84ef4ecf735b17a025bd5de904611de9ae230c9a
ilias_dimitrakopoulos <65895968+liakos1607@users.noreply.github.com>
2022-06-25 19:09:48 +0300

    Update pipeline.py

--- production-model-package/regression_model/pipeline.py
+++ production-model-package/regression_model/pipeline.py
@@ -4,116 +4,74 @@ from feature_engine.imputation import (
     CategoricalImputer,
     MeanMedianImputer,
 )
-from feature_engine.selection import DropFeatures
-from feature_engine.transformation import LogTransformer
-from feature_engine.wrappers import SklearnTransformerWrapper
-from sklearn.linear_model import Lasso
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+
+# to build the models
+from sklearn.linear_model import LogisticRegression
+
+# to evaluate the models
+from sklearn.metrics import accuracy_score, roc_auc_score
+
+# to persist the model and the scaler
+import joblib
+
+# to visualise al the columns in the dataframe
+pd.pandas.set_option('display.max_columns', None)
+
+# pipeline
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import Binarizer, MinMaxScaler
 
-from regression_model.config.core import config
-from regression_model.processing import features as pp
+#import preprocessors
+from sklearn.base import BaseEstimator, TransformerMixin
 
-price_pipe = Pipeline(
-    [
-        # ===== IMPUTATION =====
-        # impute categorical variables with string missing
-        (
-            "missing_imputation",
-            CategoricalImputer(
-                imputation_method="missing",
-                variables=config.model_config.categorical_vars_with_na_missing,
-            ),
-        ),
-        (
-            "frequent_imputation",
-            CategoricalImputer(
-                imputation_method="frequent",
-                variables=config.model_config.categorical_vars_with_na_frequent,
-            ),
-        ),
-        # add missing indicator
-        (
-            "missing_indicator",
-            AddMissingIndicator(variables=config.model_config.numerical_vars_with_na),
-        ),
-        # impute numerical variables with the mean
-        (
-            "mean_imputation",
-            MeanMedianImputer(
-                imputation_method="mean",
-                variables=config.model_config.numerical_vars_with_na,
-            ),
-        ),
-        # == TEMPORAL VARIABLES ====
-        (
-            "elapsed_time",
-            pp.TemporalVariableTransformer(
-                variables=config.model_config.temporal_vars,
-                reference_variable=config.model_config.ref_var,
-            ),
-        ),
-        ("drop_features", DropFeatures(features_to_drop=[config.model_config.ref_var])),
-        # ==== VARIABLE TRANSFORMATION =====
-        ("log", LogTransformer(variables=config.model_config.numericals_log_vars)),
-        (
-            "binarizer",
-            SklearnTransformerWrapper(
-                transformer=Binarizer(threshold=0),
-                variables=config.model_config.binarize_vars,
-            ),
-        ),
-        # === mappers ===
-        (
-            "mapper_qual",
-            pp.Mapper(
-                variables=config.model_config.qual_vars,
-                mappings=config.model_config.qual_mappings,
-            ),
-        ),
-        (
-            "mapper_exposure",
-            pp.Mapper(
-                variables=config.model_config.exposure_vars,
-                mappings=config.model_config.exposure_mappings,
-            ),
-        ),
-        (
-            "mapper_finish",
-            pp.Mapper(
-                variables=config.model_config.finish_vars,
-                mappings=config.model_config.finish_mappings,
-            ),
-        ),
-        (
-            "mapper_garage",
-            pp.Mapper(
-                variables=config.model_config.garage_vars,
-                mappings=config.model_config.garage_mappings,
-            ),
-        ),
-        # == CATEGORICAL ENCODING
-        (
-            "rare_label_encoder",
-            RareLabelEncoder(
-                tol=0.01, n_categories=1, variables=config.model_config.categorical_vars
-            ),
-        ),
-        # encode categorical variables using the target mean
-        (
-            "categorical_encoder",
-            OrdinalEncoder(
-                encoding_method="ordered",
-                variables=config.model_config.categorical_vars,
-            ),
-        ),
-        ("scaler", MinMaxScaler()),
-        (
-            "Lasso",
-            Lasso(
-                alpha=config.model_config.alpha,
-                random_state=config.model_config.random_state,
-            ),
-        ),
-    ]
+
+!pip install feature_engine
+
+# for imputation
+from feature_engine.imputation import (
+    CategoricalImputer,
+    AddMissingIndicator,
+    MeanMedianImputer)
+
+# for encoding categorical variables
+from feature_engine.encoding import (
+    RareLabelEncoder,
+    OneHotEncoder
 )
+
+# set up the pipeline
+titanic_pipe = Pipeline([
+
+    # ===== IMPUTATION =====
+    # impute categorical variables with string 'missing'
+    ('categorical_imputation', CategoricalImputer(
+    imputation_method='missing', variables=CATEGORICAL_VARIABLES)),
+    # add missing indicator to numerical variables
+    ('missing_indicator',AddMissingIndicator (variables=NUMERICAL_VARIABLES)),
+
+    # impute numerical variables with the median
+    ('median_imputation', MeanMedianImputer(imputation_method='median',
+                                       variables=NUMERICAL_VARIABLES)),
+
+
+    # Extract first letter from cabin
+    ('extract_letter', ExtractLetterTransformer(variables=CABIN)),
+
+
+    # == CATEGORICAL ENCODING ======
+    # remove categories present in less than 5% of the observations (0.05)
+    # group them in one category called 'Rare'
+    ('rare_label_encoder', RareLabelEncoder(
+        tol=0.05, n_categories=1, variables=CATEGORICAL_VARIABLES)),
+
+
+    # encode categorical variables using one hot encoding into k-1 variables
+    ('categorical_encoder',OneHotEncoder( drop_last=True, variables=CATEGORICAL_VARIABLES )),
+
+    # scale using standardization
+    ('scaler', StandardScaler ()),
+
+    # logistic regression (use C=0.0005 and random_state=0)
+    ('Logit', LogisticRegression(C=0.0005, random_state=0)),
+])
